80cfe94e00106520d65eb71fd7b3117ac4aa667f
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2022-07-28 20:20:19 +0600

    datatype

---
+++ Javascript/dataType.js
@@ -0,0 +1,26 @@
+// Learn From MDN Javascript Documentation
+
+
+
+var n1=10;
+var n1=20;
+console.log(n1)
+
+// let n2=20;
+// let n2=30;
+// console.log(n2)
+
+// const n3=30;
+// const n3=40;
+// console.log(n3)
+
+// const n3=20;
+// n3=10;
+// console.log(n3)
+
+let s="test"
+console.log(s)
+console.log(typeof(s))
+s=10;
+console.log(s)
+console.log(typeof(s))
