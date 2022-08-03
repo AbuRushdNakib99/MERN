e42f2e067f92a88dfe9afd298ed4953acc7f21fc
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2022-08-03 20:17:14 +0600

    function part 1

---
+++ Javascript/function1.js
@@ -0,0 +1,56 @@
+// Named Function
+
+function function1(){
+    console.log(Date.now())
+}
+function1()
+
+
+// Variable Function
+const now=function(){
+    console.log(Date.now())
+}
+now()
+
+// Arrow Function
+const arrowFunction=()=>{
+    console.log("test")
+}
+arrowFunction()
+
+// Inline Arrow Function
+
+const calculation=(num,fn)=>{
+    let result=0;
+    for(index=0;index<num;index++){
+        const temp=fn(index);
+        result+=temp;
+    }
+    return result
+}
+const sum=calculation(10,(x)=>{
+    return x+x
+})
+const multiply=calculation(10,(x)=>{
+    return x*x;
+})
+console.log(multiply)
+console.log(sum)
+
+// Minimal Function
+const square=(x)=>x*x;
+console.log(square(10))
+
+const square2=(x)=>{
+    const fn=function(){
+        return x*x;
+    }
+    return fn;
+}
+console.log(square2(10)())
+
+// Self Executing Function
+(()=>{return new Date();})()
+console.log((10))
+
+(function evaluateme(){console.log("this is test");return "hello"})
--- Javascript/tempCodeRunnerFile.js
+++ Javascript/tempCodeRunnerFile.js
@@ -1,7 +1,2 @@
-console.log(JSON)
-
-// var s1=JSON.parse(s)
-// console.log(s1)
-// s1.phone=123
-// s2=JSON.stringify(s1)
-// console.log(s2.length)
+(()=>{return new Date();})()
+console.log((10))
